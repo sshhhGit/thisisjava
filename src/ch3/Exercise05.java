1446cbd368a9c8e182545ba885ee19f25417ec1a
sshhhGit <seungho0497@hanmail.net>
2021-07-10 00:14:30 +0900

    Add new Exercise

---
+++ src/ch3/Exercise05.java
@@ -0,0 +1,8 @@
+package ch3;
+//3-5
+public class Exercise05 {
+    public static void main(String[] args) {
+        int value = 356;
+        System.out.println(value-(value%100));
+    }
+}
---
+++ src/ch3/Exercise06.java
@@ -0,0 +1,13 @@
+package ch3;
+//3-5
+public class Exercise06 {
+    public static void main(String[] args) {
+        int lengthTop = 5;
+        int lenghBottom = 10;
+        int height = 7;
+        double area = ((lengthTop+lengthTop)*height)/2;
+        System.out.println(area);
+
+
+    }
+}
