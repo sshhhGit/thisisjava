2c7e03628020b3625f137097925591e28f72d8d1
sshhhGit <seungho0497@hanmail.net>
2021-07-14 22:19:34 +0900

    Add new Exercise

--- src/ch13/Util.java
+++ src/ch13/Util.java
@@ -1,12 +1,15 @@
 package ch13;
 //13-4
 public class Util{
+    //방법1
     public static <K, V> V getValue(Pair<K, V> p, K k){
         if (p.getKey()==k) {
             return p.getValue();
         } else{
             return null;
         }
+
+      //방법2
 //    public static <P extends Pair<K,V>, K , V> V getValue(P p, K k) {
 //        if (p.getKey() == k) {
 //            return p.getValue();
